e210f371870879192084220443d83a91ff6a4d9c
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-09-17 07:53:25 +0300

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/drive-ukraine-now">
+      <BrowserRouter>
         <GlobalStyle/>
         <App />
       </BrowserRouter>
