fd1dc729432a6f042f328fa9c964b845aa5d4ad0
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-09-17 09:19:42 +0300

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <GlobalStyle/>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
