0a0ca4580dacc27266e8dcbd2df2f8b8e67d1093
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-09-20 20:47:52 +0300

    added sideBar to favorites

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,29 +1,111 @@
 import AdvertListItem from 'components/AdvertListItem/AdvertListItem';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, EmptyFavorites, EmptyFavoritesMessage } from './Favorites.styled';
 import { useSelector } from 'react-redux';
 import Footer from 'components/Footer/Footer';
 import { ToastContainer } from 'react-toastify';
+import FilteredForm from 'components/FilteredForm/FilteredForm';
+import AdvertList from 'components/AdvertList/AdvertList';
 
 const Favorites = () => {
   const favorites = useSelector((state) => state.favorites.favorites);
 
+  const [filters, setFilters] = useState({
+    make: '',
+    filteredPrices: [],
+    minMileage: '',
+    maxMileage: '',
+  });
+  const [filteredAdverts, setFilteredAdverts] = useState(null);
+  const [isFiltering, setIsFiltering] = useState(false);
+
+  console.log("favorites: ", favorites);
+
+  useEffect(() => {
+    if (isFiltering) {
+      if (
+        filters.make ||
+        filters.filteredPrices.length > 0 ||
+        filters.minMileage ||
+        filters.maxMileage
+      ) {
+        const filteredAdverts = favorites.filter((advert) => {
+          if (filters.make && advert.make !== filters.make.value) {
+            return false;
+          }
+          if (
+            filters.filteredPrices.length > 0 &&
+            !filters.filteredPrices.some((priceObj) => priceObj.value === advert.rentalPrice.replace('$', ''))
+          ) {
+            return false;
+          }
+          if (filters.minMileage && advert.mileage < filters.minMileage) {
+            return false;
+          }
+          if (filters.maxMileage && advert.mileage > filters.maxMileage) {
+            return false;
+          }
+          return true;
+        });
+  
+        setFilteredAdverts(filteredAdverts);
+      } else {
+        setFilteredAdverts([]);
+      }
+
+      return;
+    }
+  }, [filters, favorites, isFiltering]);
+
+  const makes = [...new Set(favorites.map((advert) => advert.make))];
+  const prices = [...new Set(favorites.map((advert) => advert.rentalPrice.replace('$', '')))];
+  const mileage = [...new Set(favorites.map((advert) => advert.mileage))];
+  const minMileage = Math.min(...mileage);
+  const maxMileage = Math.max(...mileage);
+
+  console.log("favorites:", favorites);
+console.log("filteredAdverts:", filteredAdverts);
+
   return (
     <>
       <Container>
+        <FilteredForm
+          makes={makes}
+          prices={prices}
+          minMileage={minMileage}
+          maxMileage={maxMileage}
+          onFilterChange={(newFilters) => {
+            setFilters(newFilters);
+            setIsFiltering(true);
+          }}
+          filters={filters}
+        />
         {favorites.length === 0 ? (
-          <EmptyFavorites>
-            <EmptyFavoritesMessage>Your favorites list is empty.</EmptyFavoritesMessage>
-          </EmptyFavorites>
-        ) : (
-          favorites.map((advert, index) => (
-            <div key={advert.id}>
-              <AdvertListItem advert={advert} index={index} />
-            </div>
-          ))
-        )}
+  <EmptyFavorites>
+    <EmptyFavoritesMessage>Your favorites list is empty.</EmptyFavoritesMessage>
+  </EmptyFavorites>
+) : (
+  <>
+    {isFiltering ? (
+      filteredAdverts !== null && filteredAdverts.length > 0 ? (
+        <AdvertList filteredAdverts={filteredAdverts} /> 
+      ) : (
+        <EmptyFavorites>
+          <EmptyFavoritesMessage>No matching favorites found.</EmptyFavoritesMessage>
+        </EmptyFavorites>
+      )
+    ) : (
+      favorites.map((advert, index) => (
+        <div key={advert.id}>
+          <AdvertListItem advert={advert} index={index} />
+        </div>
+      ))
+    )}
+  </>
+)}
+
       </Container>
-      <ToastContainer/>
+      <ToastContainer />
       <Footer />
     </>
   );
